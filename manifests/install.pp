e3ab2cc0b2e01cd953132aa31d17f71fd13b0573
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-12-11 16:36:05 +0100

    If RedHat, pull in epel-release

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,6 +4,11 @@
 #
 class hitch::install {
 
+  if $::osfamily == 'RedHat' {
+    ensure_packages(['epel-release'])
+    Package['epel-release'] -> Package[$::hitch::package_name]
+  }
+
   package { $::hitch::package_name:
     ensure => present,
   }
