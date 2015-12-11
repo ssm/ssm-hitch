327653c1a3fc2c583337684a4bff24d7122ba730
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-12-11 11:41:14 +0100

    Manage the PEM file to pieces

--- manifests/domain.pp
+++ manifests/domain.pp
@@ -7,8 +7,8 @@ define hitch::domain (
   $key,
   $cert,
   $ensure      = present,
-  $cacert      = '',
-  $dhparams    = '',
+  $cacert      = undef,
+  $dhparams    = undef,
 )
 {
 
@@ -31,18 +31,55 @@ define hitch::domain (
   }
 
   validate_re($ensure, ['^present$', '^absent$'])
+
   # Add a line to the hitch config file
   concat::fragment { "hitch::domain ${title}":
     target  => $config_file,
     content => "pem-file = \"${pem_file}\"",
   }
 
-  file { "/etc/hitch/${title}.pem":
-    ensure  => $ensure,
-    mode    => '0600',
-    owner   => $::hitch::user,
-    group   => $::hitch::group,
-    content => "${cacert}\n${cert}\n${key}\n${dhparams}",
+  # Create the pem file, with (optional) ca certificate chain, a
+  # certificate, a key, and finally the dh parameters
+  concat { $pem_file:
+    ensure => $ensure,
+    mode   => '0640',
+    owner  => $::hitch::file_owner,
+    group  => $::hitch::group,
   }
 
+  if $cacert {
+    concat::fragment {"${title} cacert":
+      content => $cacert,
+      target  => $pem_file,
+      order   => '01',
+    }
+  }
+
+  concat::fragment {"${title} cert":
+    content => $cert,
+    target  => $pem_file,
+    order   => '02',
+  }
+
+  concat::fragment {"${title} key":
+    content => $key,
+    target  => $pem_file,
+    order   => '03',
+  }
+
+  if $dhparams {
+    concat::fragment {"${title} dhparams":
+      content => $dhparams,
+      target  => $pem_file,
+      order   => '04',
+    }
+  }
+  else {
+    concat::fragment {"${title} dhparams":
+      source => $::hitch::dhparams_file,
+      target => $pem_file,
+      order  => '04',
+    }
+  }
+  
 }
--- spec/defines/hitch__domain_spec.rb
+++ spec/defines/hitch__domain_spec.rb
@@ -11,15 +11,24 @@ describe 'hitch::domain' do
         let(:title) { 'example.com' }
         let(:params) do
           {
-            'key' => '-----BEGIN PRIVATE KEY-----',
-            'cert' => '-----BEGIN CERTIFICATE-----',
             'cacert' => '-----BEGIN CERTIFICATE-----',
+            'cert' => '-----BEGIN CERTIFICATE-----',
+            'key' => '-----BEGIN PRIVATE KEY-----',
             'dhparams' => '-----BEGIN DH PARAMETERS-----'
           }
         end
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_hitch__domain('example.com') }
-        it { is_expected.to contain_file('/etc/hitch/example.com.pem') }
+
+        # for the pem file
+        it { is_expected.to contain_concat('/etc/hitch/example.com.pem') }
+        it { is_expected.to contain_concat__fragment('example.com cacert') }
+        it { is_expected.to contain_concat__fragment('example.com cert') }
+        it { is_expected.to contain_concat__fragment('example.com key') }
+        it { is_expected.to contain_concat__fragment('example.com dhparams') }
+
+        # for the config file
+        it { is_expected.to contain_concat('/etc/hitch/hitch.conf') }
         it { is_expected.to contain_concat__fragment('hitch::domain example.com') }
       end
     end
