fb84db9728e0c9d5fcec0729e7a72ea96ff3d3d9
Stig Sandbeck Mathisen <ssm@fnord.no>
2019-03-02 13:40:49 +0100

    Use typed parameters with defaults from module data

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,14 +2,16 @@
 #
 # This class is called from hitch for install.
 #
-class hitch::install {
+class hitch::install (
+  String $package,
+) {
 
   if $::osfamily == 'RedHat' {
     ensure_packages(['epel-release'])
-    Package['epel-release'] -> Package[$::hitch::package_name]
+    Package['epel-release'] -> Package[$package]
   }
 
-  package { $::hitch::package_name:
+  package { $package:
     ensure => present,
   }
 }
