05f26a20d81d3fe2add61016a4279ac988a5dc9b
Stig Sandbeck Mathisen <ssm@fnord.no>
2019-10-12 11:16:44 +0200

    Add test for missing dhparams content
    This should fail until #6 is fixed.

--- spec/classes/hitch_spec.rb
+++ spec/classes/hitch_spec.rb
@@ -5,7 +5,24 @@ describe 'hitch' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+      context 'defaults' do
+        it { is_expected.to compile }
+        it { is_expected.to contain_exec('hitch::config generate dhparams') }
+        it {
+          is_expected.to contain_file('/etc/hitch/dhparams.pem')
+            .without_content
+        }
+      end
+
+      context 'with dhparams_content' do
+        let(:params) { { dhparams_content: 'BEGIN DH PARAMETERS' } }
+
+        it { is_expected.not_to contain_exec('hitch::config generate dhparams') }
+        it {
+          is_expected.to contain_file('/etc/hitch/dhparams.pem')
+            .with_content(%r{BEGIN DH PARAMETERS})
+        }
+      end
     end
   end
 end
